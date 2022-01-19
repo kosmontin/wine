8ce6f2aed08642d350fc13bee41849ab1b0f478e
Konstantin Beloborodov <kosta.white@gmail.com>
2022-01-19 13:43:31 +0300

    Step 11 of 17. Add Excel file and modify template.html

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from datetime import datetime
+import pandas
+
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -9,9 +11,13 @@ env = Environment(
 
 template = env.get_template('template.html')
 
+xls_data = pandas.read_excel('db/wine.xlsx', usecols=['Название', 'Сорт', 'Цена', 'Картинка'])
+
 rendered_page = template.render(
-    age = datetime.now().year - 1920
+    age = datetime.now().year - 1920,
+    cards = xls_data.to_dict(orient='records')
 )
+
 
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
