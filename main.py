aa148d2b2095383c5768824eca186c7f504c107d
Konstantin Beloborodov <kosta.white@gmail.com>
2022-01-24 12:41:46 +0300

    BUGFIX #7: main.py was modified according suggesed structure

--- main.py
+++ main.py
@@ -7,37 +7,46 @@ import os
 import pandas
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-template = env.get_template('template.html')
-
-parser = argparse.ArgumentParser(
-    description='''Local web-server and site about wine. 
-    After run, open in browser http://127.0.0.1:8000/''')
-parser.add_argument('--file', type=str, default='db/data.xlsx', help='excel file for load data on site from it')
-args = parser.parse_args()
-
-FILE_PATH = args.file
 FOUNDING_DATE = 1920
 
-if os.path.isfile(FILE_PATH):
-    products_from_xls = pandas.read_excel(FILE_PATH, na_values=' ', keep_default_na=False)
-    wines = defaultdict(list)
-    for wine in products_from_xls.values:
-        wines[wine[0]].append(dict(zip(list(products_from_xls), wine)))
 
-    rendered_page = template.render(
-        age=datetime.now().year - FOUNDING_DATE,
-        wines=OrderedDict(sorted(wines.items()))
+def get_filepath_from_args():
+    parser = argparse.ArgumentParser(
+        description='''Local web-server and site about wine. 
+            After run, open in browser http://127.0.0.1:8000/''')
+    parser.add_argument('--file', type=str, default='db/data.xlsx', help='excel file for load data on site from it')
+    args = parser.parse_args()
+    return args.file
+
+
+def main():
+    filepath = get_filepath_from_args()
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
     )
 
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    template = env.get_template('template.html')
 
-    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-    server.serve_forever()
-else:
-    print(f'File {FILE_PATH} not found. Please, check path and try again.')
+    if os.path.isfile(filepath):
+        products_from_xls = pandas.read_excel(filepath, na_values=' ', keep_default_na=False)
+        wines = defaultdict(list)
+        for wine in products_from_xls.values:
+            wines[wine[0]].append(dict(zip(list(products_from_xls), wine)))
+
+        rendered_page = template.render(
+            age=datetime.now().year - FOUNDING_DATE,
+            wines=OrderedDict(sorted(wines.items()))
+        )
+
+        with open('index.html', 'w', encoding="utf8") as file:
+            file.write(rendered_page)
+
+        server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+        server.serve_forever()
+    else:
+        print(f'File {filepath} not found. Please, check path and try again.')
+
+
+if __name__ == '__main__':
+    main()
